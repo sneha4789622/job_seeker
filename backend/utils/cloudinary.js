79cb4f2c2b1e2921dbabbd2985ba7803c684ef5c
Sneha Bharti <sneha.bharti114@gmail.com>
2026-01-07 23:33:38 +0530

    updated

--- backend/utils/cloudinary.js
+++ backend/utils/cloudinary.js
@@ -3,14 +3,8 @@ import dotenv from "dotenv";
 dotenv.config();
 
 cloudinary.config({
-<<<<<<< HEAD
     cloud_name: process.env.CLOUD_NAME,
     api_key: process.env.API_KEY,
     api_secret: process.env.API_SECRET,
-=======
-    cloud_name:process.env.CLOUD_NAME,
-    api_key:process.env.API_KEY,
-    api_secret:process.env.API_SECRET
->>>>>>> 99c2990f774df0329fafe0f462d72128dc74cb71
 });
 export default cloudinary;
